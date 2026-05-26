140863549af1473e8f6532561f5ffdd15301abad
Maple_LeavesY <shishenxiao1@gmail.com>
2026-05-26 19:48:30 +0800

    更新

--- Assets/Spript/ObjectSpripts/ClearCount.cs
+++ Assets/Spript/ObjectSpripts/ClearCount.cs
@@ -4,12 +4,55 @@ public class ClearCount : MonoBehaviour
 {
     [SerializeField] private Transform _countTopPoint;
     [SerializeField] private KitchenObjectSO _kitchenObjectSO;
+    [SerializeField] private ClearCount _secondClearCount;
+    [SerializeField] private bool testing;
+
+    private KitchenObject _kitchenObject;
+    private void Update()
+    {
+        if(testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if(_kitchenObject != null)
+            {
+                _kitchenObject.SetClearCounter(_secondClearCount);
+                Debug.Log(_kitchenObject.GetClearCounter());
+            }
+        }
+    }
     public void Interact()
     {
-        Debug.Log("Interact!");
-        Transform kitchenObjectTransform = Instantiate(_kitchenObjectSO.prefab, _countTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        if(_kitchenObject == null)
+        {
+            Debug.Log("Interact!");
+            Transform kitchenObjectTransform = Instantiate(_kitchenObjectSO.prefab, _countTopPoint);
+            kitchenObjectTransform.localPosition = Vector3.zero;
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+            _kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            _kitchenObject.SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(_kitchenObject.GetClearCounter());
+        }
+    }
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return _countTopPoint;
+    }
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        _kitchenObject = kitchenObject;
+    }
+    public KitchenObject GetKitchenObject(KitchenObject kitchenObject)
+    {
+        return _kitchenObject;
+    }
+    public void ClearKitchenObject()
+    {
+        _kitchenObject = null;
+    }
+    public bool HasKitchenObject()
+    {
+        return _kitchenObject != null;
     }
 }
--- Assets/Spript/ObjectSpripts/ScriptableObject/KitchenObject.cs
+++ Assets/Spript/ObjectSpripts/ScriptableObject/KitchenObject.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO _kitchenObjectSO;
-
+    
+    private ClearCount _clearCount;
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return _kitchenObjectSO;
     }
+    public void SetClearCounter(ClearCount clearCount)
+    {
+        _clearCount = clearCount;
+        
 
+    }
+    public ClearCount GetClearCounter()
+    {
+        return _clearCount;
+    }
 }
+ 
